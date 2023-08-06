5e43ac98fc69aa1644d94d9a16d322e5b449f40f
Akeem09 <138660764+Akeem09@users.noreply.github.com>
2023-08-06 22:18:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,527 @@
+var dataSet=[
+    {
+      "id": 1,
+      "foodname": "Apple",
+      "calorie": 95,
+      "category": "Fruit",
+      "protiens": 0.3,
+      "cab": 25
+    },
+    {
+      "id": 2,
+      "foodname": "Banana",
+      "calorie": 105,
+      "category": "Fruit",
+      "protiens": 1.3,
+      "cab": 27
+    },
+    {
+      "id": 3,
+      "foodname": "Orange",
+      "calorie": 62,
+      "category": "Fruit",
+      "protiens": 1.2,
+      "cab": 15
+    },
+    {
+      "id": 4,
+      "foodname": "Pear",
+      "calorie": 95,
+      "category": "Fruit",
+      "protiens": 0.5,
+      "cab": 25
+    },
+    {
+      "id": 5,
+      "foodname": "Grapefruit",
+      "calorie": 42,
+      "category": "Fruit",
+      "protiens": 1.1,
+      "cab": 11
+    },
+    {
+      "id": 6,
+      "foodname": "Strawberry",
+      "calorie": 46,
+      "category": "Fruit",
+      "protiens": 1.5,
+      "cab": 10
+    },
+    {
+      "id": 7,
+      "foodname": "Blueberry",
+      "calorie": 52,
+      "category": "Fruit",
+      "protiens": 1.1,
+      "cab": 14
+    },
+    {
+      "id": 8,
+      "foodname": "Raspberry",
+      "calorie": 52,
+      "category": "Fruit",
+      "protiens": 1.4,
+      "cab": 15
+    },
+    {
+      "id": 9,
+      "foodname": "Broccoli",
+      "calorie": 34,
+      "category": "Vegetable",
+      "protiens": 3.3,
+      "cab": 5
+    },
+    {
+      "id": 10,
+      "foodname": "Cauliflower",
+      "calorie": 25,
+      "category": "Vegetable",
+      "protiens": 2.6,
+      "cab": 5
+    },
+    {
+      "id": 11,
+      "foodname": "Green Beans",
+      "calorie": 31,
+      "category": "Vegetable",
+      "protiens": 2.4,
+      "cab": 4
+    },
+    {
+      "id": 12,
+      "foodname": "Asparagus",
+      "calorie": 20,
+      "category": "Vegetable",
+      "protiens": 2.2,
+      "cab": 2
+    },
+    {
+      "id": 13,
+      "foodname": "Spinach",
+      "calorie": 23,
+      "category": "Vegetable",
+      "protiens": 3.5,
+      "cab": 4
+    },
+    {
+      "id": 14,
+      "foodname": "Kale",
+      "calorie": 33,
+      "category": "Vegetable",
+      "protiens": 4.3,
+      "cab": 7
+    },
+    {
+      "id": 15,
+      "foodname": "Sweet Potato",
+      "calorie": 103,
+      "category": "Vegetable",
+      "protiens": 2,
+      "cab": 27
+    },
+    {
+      "id": 16,
+      "foodname": "Potato",
+      "calorie": 161,
+      "category": "Vegetable",
+      "protiens": 4.3,
+      "cab": 37
+    },
+    {"id": 17,
+      "foodname": "Carrot",
+      "calorie": 41,
+      "category": "Vegetable",
+      "protiens": 0.9,
+      "cab": 9
+    },
+    {
+      "id": 18,
+      "foodname": "Onion",
+      "calorie": 40,
+      "category": "Vegetable",
+      "protiens": 1.4,
+      "cab": 9
+    },
+    {
+      "id": 19,
+      "foodname": "Egg",
+      "calorie": 77,
+      "category": "Protein",
+      "protiens": 6.3,
+      "cab": 0.5
+    },
+    {
+      "id": 20,
+      "foodname": "Chicken Breast",
+      "calorie": 165,
+      "category": "Protein",
+      "protiens": 31,
+      "cab": 0
+    },
+    {
+      "id": 21,
+      "foodname": "Salmon",
+      "calorie": 206,
+      "category": "Protein",
+      "protiens": 22,
+      "cab": 0
+    },
+    {
+      "id": 22,
+      "foodname": "Tuna",
+      "calorie": 179,
+      "category": "Protein",
+      "protiens": 39,
+      "cab": 0
+    },
+    {
+      "id": 23,
+      "foodname": "Beef",
+      "calorie": 250,
+      "category": "Protein",
+      "protiens": 26,
+      "cab": 0
+    },
+    {
+      "id": 24,
+      "foodname": "Pork",
+      "calorie": 242,
+      "category": "Protein",
+      "protiens": 26,
+      "cab": 0
+    },
+    {
+      "id": 25,
+      "foodname": "Lamb",
+      "calorie": 294,
+      "category": "Protein",
+      "protiens": 25,
+      "cab": 0
+    },
+    {
+      "id": 26,
+      "foodname": "Shrimp",
+      "calorie": 85,
+      "category": "Protein",
+      "protiens": 20,
+      "cab": 0.2
+    },
+    {
+      "id": 27,
+      "foodname": "Quinoa",
+      "calorie": 120,
+      "category": "Grain",
+      "protiens": 4.4,
+      "cab": 21
+    },
+    {
+      "id": 28,
+      "foodname": "Brown Rice",
+      "calorie": 216,
+      "category": "Grain",
+      "protiens": 4.5,
+      "cab": 45
+    },
+    {
+      "id": 29,
+      "foodname": "Oats",
+      "calorie": 389,
+      "category": "Grain",
+      "protiens": 16.9,
+      "cab": 66
+    },
+    {
+      "id": 30,
+      "foodname": "Quinoa",
+      "calorie": 120,
+      "category": "Grain",
+      "protiens": 4.4,
+      "cab": 21
+    },
+    {
+      "id": 31,
+      "foodname": "Bread",
+      "calorie": 265,
+      "category": "Grain",
+      "protiens": 9.4,
+      "cab": 49
+    },
+    {
+      "id": 32,
+      "foodname": "Pasta",
+      "calorie": 131,
+      "category": "Grain",
+      "protiens": 5.5,
+      "cab": 26
+    },
+    {
+      "id": 33,
+      "foodname": "Milk",
+      "calorie": 60,
+      "category": "Dairy",
+      "protiens": 3.2,
+      "cab": 5.1
+    },
+    {
+      "id": 34,
+      "foodname": "Cheese",
+      "calorie": 402,
+      "category": "Dairy",
+      "protiens": 25,
+      "cab": 2.4
+    },
+    {
+      "id": 35,
+      "foodname": "Yogurt",
+      "calorie": 59,
+      "category": "Dairy",
+      "protiens": 3.5,
+      "cab": 5
+    },
+    {
+      "id": 36,
+      "foodname": "Butter",
+      "calorie": 717,
+      "category": "Dairy",
+      "protiens": 0.9,
+      "cab": 0.1
+    },
+    {
+      "id": 37,
+      "foodname": "Almonds",
+      "calorie": 579,
+      "category": "Nuts",
+      "protiens": 21,
+      "cab": 22
+    },
+    {
+      "id": 38,
+      "foodname": "Walnuts",
+      "calorie": 654,
+      "category": "Nuts",
+      "protiens": 15,
+      "cab": 14
+    },
+    {
+      "id": 39,
+      "foodname": "Peanuts",
+      "calorie": 567,
+      "category": "Nuts",
+      "protiens": 26,
+      "cab": 16
+    },
+    {
+      "id": 40,
+      "foodname": "Cashews",
+      "calorie": 553,
+      "category": "Nuts",
+      "protiens": 18,
+      "cab": 30
+    }
+  ]
+
+  // all food items
+
+  console.log("All FOOD ITEMS are mentioned below: \n");
+
+  const foodItems=(food)=>{
+    return food.foodname;
+  }
+
+  var items=dataSet.map(foodItems);
+  console.log(items)
+
+  // food items with category vegetable
+
+  console.log("\n","All Food Items with category VEGETABLE are mentioned below: \n");
+
+  const vegetable=dataSet.filter((food)=>{
+    if(food.category=="Vegetable"){
+        return true;
+    }
+    else
+        return false;
+  })
+
+const vegetableName=vegetable.map((vegetable)=>{
+    return vegetable.foodname;
+})
+
+console.log(vegetableName)
+
+// food items with category fruit
+
+console.log("\n","All Food Items with category FRUIT are mentioned below: \n");
+
+const fruit=dataSet.filter((food)=>{
+    if(food.category=="Fruit"){
+        return true;
+    }
+    else
+        return false;
+  })
+
+const fruitName=fruit.map((fruit)=>{
+    return fruit.foodname;
+})
+
+console.log(fruitName)
+
+// food items with category protien
+
+console.log("\n","All Food Items with category PROTEIN are mentioned below: \n");
+
+const protein=dataSet.filter((food)=>{
+  if(food.category=="Protein"){
+      return true;
+  }
+  else
+      return false;
+})
+
+const proteinName=protein.map((protein)=>{
+  return protein.foodname;
+})
+
+console.log(proteinName)
+
+// food items with category nuts
+
+console.log("\n","All Food Items with category NUTS are mentioned below: \n");
+
+const nuts=dataSet.filter((food)=>{
+  if(food.category=="Nuts"){
+    return true;
+  }
+  else{
+    return false;
+  }
+})
+
+const nutsName=nuts.map((nut)=>{
+  return nut.foodname;
+})
+
+console.log(nutsName)
+
+// food items with category grains
+
+console.log("\n","All Food Items with category GRAINS are mentioned below: \n");
+
+const grains=dataSet.filter((food)=>{
+  if(food.category=="Grain"){
+    return true;
+  }
+  else{
+    return false;
+  }
+})
+
+const grainsName=grains.map((grain)=>{
+  return grain.foodname;
+})
+
+console.log(grainsName);
+
+// food items with category dairy
+
+console.log("\n","All Food Items with category DAIRY are mentioned below: \n");
+
+const dairyProducts=dataSet.filter((food)=>{
+  if(food.category=="Dairy"){
+    return true;
+  }
+  else{
+    return false;
+  }
+})
+
+const dairyProductsName=dairyProducts.map((dairyProduct)=>{
+  return dairyProduct.foodname;
+})
+
+console.log(dairyProductsName)
+
+// food items with high calorie content
+
+console.log("\n","All Food Items with HIGH CALORIE CONTENT are mentioned below: \n");
+
+const highCalorieProducts=dataSet.filter((food)=>{
+  if(food.calorie>"100"){
+    return true;
+  }
+  else{
+    return false;
+  }
+})
+
+const highCalorieProductsName=highCalorieProducts.map((highCalorieProduct)=>{
+  var obj={};
+  obj.foodname=highCalorieProduct.foodname;
+  obj.calorie=highCalorieProduct.calorie;
+  return obj;
+})
+
+console.log(highCalorieProductsName)
+
+// food items with low calorie content
+
+console.log("\n","All Food Items with LOW CALORIE CONTENT are mentioned below: \n");
+
+const lowCalorieProducts=dataSet.filter((food)=>{
+  if(food.calorie<"100"){
+    return true;
+  }
+  else{
+    return false;
+  }
+})
+
+const lowCalorieProductsName=lowCalorieProducts.map((lowCalorieProduct)=>{
+  var obj={};
+  obj.foodname=lowCalorieProduct.foodname;
+  obj.calorie=lowCalorieProduct.calorie;
+  return obj;
+})
+
+console.log(lowCalorieProductsName)
+
+// food items with highest protein content to lowest
+
+console.log("\n","Food items with HIGHEST PROTIEN CONTENT to LOWEST \n")
+
+const proteinContent=dataSet.map((food)=>{
+  var obj={};
+  obj.foodname=food.foodname;
+  obj.protiens=food.protiens;
+  return obj;
+})
+
+const compare=(a,b)=>{
+  return b.protiens-a.protiens;
+}
+
+proteinContent.sort(compare)
+
+console.log(proteinContent);
+
+
+
+// food items with lowest cab content to highest
+
+console.log("\n","Food items with LOWEST CAB CONTENT to HIGHEST \n")
+
+const cabContent=dataSet.map((food)=>{
+  var obj={};
+  obj.foodname=food.foodname;
+  obj.cab=food.cab;
+  return obj;
+})
+
+const comparison=(a,b)=>{
+  return a.cab-b.cab;
+}
+
+cabContent.sort(comparison);
+
+console.log(cabContent);
